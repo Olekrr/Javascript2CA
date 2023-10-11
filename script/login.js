14fd1c453748bc9b671395d0b50864cb682719fe
Olekrr <93515165+Olekrr@users.noreply.github.com>
2023-10-11 04:56:19 +0200

    posts page, jsdocs, code cleaning
    Created post page with all functionality,
    Cleaned up code a bit
    Added JSdocs

--- script/login.js
+++ script/login.js
@@ -1,19 +1,35 @@
-import { login } from './api.mjs';
+import { login } from "./api.mjs";
 
+/**
+ * Event listener function for the login form.
+ *
+ * @async
+ * @param {Event} event - The submit event.
+ * @throws {Error} - Alerts the error message if any occurs during the login process.
+ */
 document.querySelector("#pills-login form").addEventListener("submit", async (event) => {
     event.preventDefault();
 
+    /** @type {string} */
     const email = document.querySelector("#loginEmail").value;
+
+    /** @type {string} */
     const password = document.querySelector("#loginPassword").value;
 
     try {
-        const data = await login(email, password);
+        /** @type {Object} */
+        const userResponse = await login(email, password);
 
-        localStorage.setItem("accessToken", data.accessToken);
-        localStorage.setItem("userEmail", data.email);
-        localStorage.setItem("userName", data.name);
+        /** Destructure relevant properties from the response */
+        const { accessToken, email: userEmail, name } = userResponse;
 
-        alert("Logged in successfully!");
+        /** Store user details in local storage */
+        localStorage.setItem("accessToken", accessToken);
+        localStorage.setItem("userEmail", userEmail);
+        localStorage.setItem("userName", name);
+
+        /** Redirect to posts page */
+        window.location.href = "/posts/index.html";
     } catch (error) {
         alert("An error occurred: " + error.message);
     }
